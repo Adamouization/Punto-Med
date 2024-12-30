cb58dc352fb19563b9c0e6ae5dbfa76c06bf0554
Adam GlobeFlix <critter@globeflix.app>
2024-12-30 18:51:27 +0100

    Use google analytics from next/third-parties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { type Metadata } from 'next'
 
 import { RootLayout } from '@/components/RootLayout'
-import { GoogleTagManager } from '@/components/custom/GoogleTagManager'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 import '@/styles/tailwind.css'
 
@@ -15,12 +15,10 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
-      <head>
-        <GoogleTagManager />
-      </head>
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
       </body>
+      <GoogleAnalytics gaId="G-T881Q1ZE99" />
     </html>
   )
 }
